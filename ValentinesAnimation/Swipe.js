e3131b0bd1c6cef6c198a5e65fee18a104de3f31
zhuqingguang <vac872089248@gmail.com>
2017-06-27 20:44:34 +0800

    完成基本的布局和动画

--- ValentinesAnimation/Swipe.js
+++ ValentinesAnimation/Swipe.js
@@ -1,5 +1,5 @@
 /////////
-//实现页面华东//
+//实现页面滑动//
 /////////////
 
 /**
@@ -7,27 +7,27 @@
  * @param {Object} container the container which will be scrolled
  * @return {Object} 滑动的对象
  */
- function Swipe(container){
-   // 获取第一个子节点
-   var element = container.find(':first');
+function Swipe(container) {
+    // 获取第一个子节点
+    var element = container.find(':first');
 
-   // 滑动的对象
-   var swipe = {};
+    // 滑动的对象
+    var swipe = {};
 
-   // li页面的数量
-   var slides = element.find('li');
+    // li页面的数量
+    var slides = element.find('li');
 
-   // 获取容器的尺寸
-   var width = container.width();
-   var height = container.height();
+    // 获取容器的尺寸
+    var width = container.width();
+    var height = container.height();
 
-   // 设置li页面的总宽度
-   element.css({
-     width: (slides.length * width) + 'px',
-     height: height + 'px'
-   });
+    // 设置li页面的总宽度
+    element.css({
+        width: (slides.length * width) + 'px',
+        height: height + 'px'
+    });
 
-   //设置每一个页面li的宽度
+    //设置每一个页面li的宽度
     $.each(slides, function(index) {
         var slide = slides.eq(index); //获取到每一个li元素
         slide.css({
@@ -37,15 +37,15 @@
     });
 
     //监控完成与移动
-   swipe.scrollTo = function(x, speed) {
-       //执行动画移动
-       element.css({
-           'transition-timing-function' : 'linear',
-           'transition-duration'        : speed + 'ms',
-           'transform'                  : 'translate3d(-' + x + 'px,0px,0px)'
-       });
-       return this;
-   };
+    swipe.scrollTo = function(x, speed) {
+        //执行动画移动
+        element.css({
+            'transition-timing-function': 'linear',
+            'transition-duration': speed + 'ms',
+            'transform': 'translate3d(-' + x + 'px,0px,0px)'
+        });
+        return this;
+    };
 
-   return swipe;
- }
+    return swipe;
+}
--- ValentinesAnimation/log.md
+++ ValentinesAnimation/log.md
@@ -16,4 +16,55 @@ element.css({
 ```
 > note:*transform属性是静态属性，不是动画属性，一旦写到style里面，将会直接显示作用，无任何变化过程*
 
+### 添加任务和第一个场景的背景
+第一个场景的背景分为三个部分:上方-楼房 中间-路 下方-绿地，实际上是用三个div依次实现的。
+人物的div使用``position: absolution``，以达到可以在各个场景中穿梭的效果。
+人物一开始是在中间的路上走，当页面的大小发生变化时，需要动态调整人物的高度，通过检测中间路的位置来更改人物的位置
+
+- 获取路的位置
+ ```
+var getValue = function(className) {
+                var $elem = $('' + className + '');
+                // 走路的路线
+                return {
+                    height: $elem.height(),
+                    top: $elem.position().top
+                };
+            }
+            // 路的y轴
+var pathY = function() {
+    var data = getValue('.a_background_middle');
+    return data.top + data.height / 2;
+}();
+ ```
+
+- 设置任务的位置
+```
+// 25是一个偏移值
+$boy.css({
+    top: pathY - boyHeight + 25
+});
+```
+
+### 精灵动画实现人物的走路效果
+CSS Sprites在国内很多人叫CSS精灵，其实这个技术不新鲜，原理就是：**靠不断的切换图片让人感觉视觉上不断在变化**，例如gif动画之类的效果。
+可以使用CSS3的动画效果来切换div的``background-position``属性。
+然后使用了``jquery.transit``来实现transition的CSS3过渡动画
+```
+$boy.transition({
+    'left': $("#content").width() + 'px',
+}, 10000, 'linear', function() {});
+```
+
+### 路径动画的处理
+路径动画就是人物行走的路径和在时刻y轴的不同位置
+- 走路到页面的2/3的时候，主题页面开始滑动
+- 走路到1/2的时候，到了商店门口
+- 进出商店
+- 走路到1/4到了桥边
+- 走路到1/2到了桥上
+
+### 本案例中使用了jQuery中的``dererred``来实现人物依次连续的动画。
+其中人物走动的动画完成后会依次进行其他动画，开关门的动画也是依次实现的，先开门后关门
+
 
